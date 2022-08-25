fd9e265b0cff7515621efc1880247ad1203345f1
preegnees <mrraziapov13@gmail.com>
2022-08-25 21:01:55 +0300

    функция для получения ссылки на оплату

---
+++ pay.py
@@ -0,0 +1,16 @@
+from yoomoney import Quickpay
+from loguru import logger
+
+def get_url(receiver: str, label: str, sum: str) -> str:
+    logger.debug(f"label={label}, sum={sum}")
+    quickpay = Quickpay(
+        receiver=receiver,
+        quickpay_form="shop",
+        targets=f"VPN subscription: {label}",
+        paymentType="",
+        sum=sum,
+        label=label
+    )
+    url = quickpay.redirected_url
+    logger.debug(url)
+    return url
